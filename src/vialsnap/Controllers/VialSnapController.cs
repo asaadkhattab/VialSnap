1826642ab68c49d30897850b593969faedcfcb15
Asaad Khattab <asaad1592@hotmail.com>
2017-12-14 20:01:43 -0500

    Added VialSnap Controlelr

---
+++ src/vialsnap/Controllers/VialSnapController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vialsnap.Controllers
+{
+    public class VialSnapController : Controller
+    {
+    }
+}
