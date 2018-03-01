b39a45cbcba858435a197682d927067b7623e24c
Asaad Khattab <asaad@khattab.live>
2018-02-28 20:46:23 -0500

    Added Database initializer

--- src/Portal/PatientConsoleModel/Context.cs
+++ src/Portal/PatientConsoleModel/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,23 @@ namespace PatientConsoleModel
         public Context()
         {
             //When we change the database
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DatabaseInitializer());
         }
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Basic> Basics { get; set; }
+
+
+        //Override
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Patient>()
+                .Property(p => p.PatientWeight)
+                .HasPrecision(4, 4);
+
+            
+        }
     }
 
 
